856c8b5e349cd232eb217e264013d75a77970e87
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-03-27 18:23:39 +0530

    Update: home page

--- src/pages/homePage/home.js
+++ src/pages/homePage/home.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { Navbar } from "../../components";
+import "./home.css";
 
 export const Home = () => {
-  return <div>home fasdfasdf dfadsf asdf adsfasdf adsfasdfdsf adsfasdffa</div>;
+  return (
+    <div>
+      <Navbar />
+      <section className="main-box">
+        {/* <Filter /> */}
+        <main className="main-product">
+          <div className="grid-three">
+            {/* {newFilterData.map((product) => (
+              <ProductCard product={product} key={product._id} />
+            ))} */}
+            <div>
+              helo asdfadfadsf asdfadfadsfdfasd fadsf asdfasfasdafgafgerg
+              agarrgadf adsf adf asd
+            </div>
+          </div>
+        </main>
+      </section>
+    </div>
+  );
 };
