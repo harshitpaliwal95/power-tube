0a7b1fe8a55a9a0f9c784534be2eb3ec04c33ebe
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-03-27 16:24:49 +0530

    Add: basic routing & add home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
-
+import { Home } from "./pages/homePage/home";
+import { Routes, Route, Link } from "react-router-dom";
+import Mockman from "mockman-js";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/mockman" element={<Mockman />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/homePage/home.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Home = () => {
+  return <div>home fasdfasdf dfadsf asdf adsfasdf adsfasdfdsf adsfasdffa</div>;
+};
