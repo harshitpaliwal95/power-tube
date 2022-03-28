0dba3c32bf53a0f2519fba08e71ecf783dff64be
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-03-28 12:23:23 +0530

    Feat: add basic routing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,7 @@
 export { Home } from "./homePage/home";
+export { Login } from "./authPage/login";
+export { SignUp } from "./authPage/signup";
+export { Explore } from "./explore/explore";
+export { Playlist } from "./playlistPage/playlist";
+export { WatchLater } from "./watchLaterPage/watchlater";
+export { LikedVideo } from "./likedvideoPage/likedvideo";
