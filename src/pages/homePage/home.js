ec95abdd170baba1978e1bf688011c458fb3585e
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-04-02 17:29:44 +0530

    Feat: fetch video card on home page

--- src/pages/homePage/home.js
+++ src/pages/homePage/home.js
@@ -1,21 +1,38 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import { Navbar, Sidebar, Card } from "../../components";
+import axios from "axios";
 
 import "./home.css";
 
 export const Home = () => {
+  const [videos, setVideos] = useState([]);
+  const [loader, setLoader] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoader(true);
+        const response = await axios.get("/api/videos");
+        setVideos(response.data.videos);
+        setLoader(false);
+      } catch (e) {
+        console.log(e.message);
+      }
+    })();
+  }, []);
+
+  console.log(...videos);
   return (
     <div>
       <Navbar />
       <section className="main-box">
         <Sidebar />
         <main className="main-product">
+          {loader && <div className="heading-lg">....loading</div>}
           <div className="grid-three">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            {videos.map((video) => (
+              <Card key={video._id} video={video} />
+            ))}
           </div>
         </main>
       </section>
