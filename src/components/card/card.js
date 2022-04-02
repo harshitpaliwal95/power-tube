fd35ff6e45649897f246f0c8aef558e822af31de
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-04-02 17:30:32 +0530

    Update: card componet

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,18 +1,16 @@
 import "./card.css";
 
-export const Card = () => {
+export const Card = ({ video }) => {
+  const { title, anime, _id } = video;
+  const videoLink = `https://i.ytimg.com/vi/${_id}/hqdefault.jpg`;
   return (
     <div className="card-component">
       <div className="card-comp-img">
-        <img
-          className="card-top-img"
-          src="https://i.ytimg.com/vi/d1N-W2BLlRs/hq720.jpg"
-          alt="loading"
-        />
+        <img className="card-top-img" src={videoLink} alt="loading" />
       </div>
       <div className="card-comp-contant">
-        <p className="text-lg">[ONE PIECE AMV] - SURVIVOR</p>
-        <p className="gray-text">Category One piece</p>
+        <p className="text-lg">{title}</p>
+        <p className="gray-text">Category {anime}</p>
       </div>
       <div className="card-btn-footer">
         <div className="card-icon-btn-right">
