66ded1e32a7eda748e2dadfe19bdf017f3114f5c
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-04-03 18:23:55 +0530

    Fix: deploay issue

--- src/utils/setCategory.js
+++ src/utils/setCategory.js
@@ -1,7 +1,5 @@
 export const setCategory = (video, category) => {
-  let newCategory = [];
   for (const name in category) {
-    // console.log(name);
     if (category[name]) {
       return video.filter((video) => video.category === name);
     }
