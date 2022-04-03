6c805c973e6fd57e86f38d8ebedf813e0ff5093b
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-04-03 15:48:29 +0530

    Add: chips on explore page

--- src/pages/explore/explore.js
+++ src/pages/explore/explore.js
@@ -1,13 +1,41 @@
+import { useState, useEffect } from "react";
 import { Navbar, Sidebar, Card } from "../../components";
+import "./explore.css";
+import axios from "axios";
 
 export const Explore = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get("/api/videos");
+        setVideos(response.data.videos);
+      } catch (e) {
+        console.log(e.message);
+      }
+    })();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <section className="main-box">
         <Sidebar />
         <main className="main-product">
-          <div className="grid-three"></div>
+          <div className="chip-container">
+            <span className="chips">All</span>
+            <span className="chips">One Piece</span>
+            <span className="chips">Naruto</span>
+            <span className="chips">Demon Slayer</span>
+            <span className="chips">Jujutsu kaisain</span>
+          </div>
+
+          <div className="grid-three">
+            {videos.map((video) => (
+              <Card key={video._id} video={video} />
+            ))}
+          </div>
         </main>
       </section>
     </div>
