3e325b8ccff18b66cb4907d2d02e90d7abb92c48
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-04-07 01:24:09 +0530

    feat: sign-up auth

--- src/pages/authPage/signup.js
+++ src/pages/authPage/signup.js
@@ -1,53 +1,92 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Navbar } from "../../components";
+import { useState } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./auth.css";
+
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const signupHandler = async () => {
+    const body = {
+      email: email,
+      password: password,
+      name: name,
+      lastName: "heyhey",
+    };
+    console.log(body);
+    try {
+      const response = await axios.post("/api/auth/signup", body);
+      localStorage.setItem("token", response.data.encodedToken);
+      console.log(response.data.encodedToken);
+      toast.success("Sign Up Succesfully");
+      setTimeout(() => {
+        navigate("/login");
+      }, 1000);
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to SignUp");
+    }
+  };
+
   return (
     <div>
       <Navbar />
       <main>
+        <ToastContainer />
         <div className="form-container">
           <div className="form-info">
             <p className="heading-x-lg text-center">Join-us</p>
             <div className="form-input">
-              <form>
-                <div className="input-box">
-                  <label className="text-medium">Full name</label>
-                  <input
-                    type="text"
-                    name="userName"
-                    placeholder="Monkey D luffy"
-                  />
-                </div>
-                <div className="input-box">
-                  <label className="text-medium">Email address</label>
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="demo@yahoo.com"
-                  />
-                </div>
-                <div className="input-box">
-                  <label className="text-medium">Password</label>
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="*******"
-                  />
-                </div>
-                <div className="space-between forget-pass">
-                  <span>
-                    <label className="text-medium">
-                      <input type="checkbox" /> I accept all term & condition
-                    </label>
-                  </span>
-                </div>
-                <div className="form-btn">
-                  <button type="submit" className="btn btn-outline">
-                    Sign Up
-                  </button>
-                </div>
-              </form>
+              <div className="input-box">
+                <label className="text-medium">Full name</label>
+                <input
+                  onChange={(e) => setName(e.target.value)}
+                  type="text"
+                  name="name"
+                  placeholder="Monkey D luffy"
+                />
+              </div>
+              <div className="input-box">
+                <label className="text-medium">Email address</label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="demo@yahoo.com"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="input-box">
+                <label className="text-medium">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="*******"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="space-between forget-pass">
+                <span>
+                  <label className="text-medium">
+                    <input type="checkbox" /> I accept all term & condition
+                  </label>
+                </span>
+              </div>
+              <div className="form-btn">
+                <button
+                  className="btn btn-outline"
+                  onClick={() => signupHandler()}
+                >
+                  Sign Up
+                </button>
+              </div>
+
               <div className="new-ac">
                 <Link to="/login">
                   <p className="text-center">
