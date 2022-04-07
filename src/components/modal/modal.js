f9d91b1e7413efd40f684d4baf0c5d492063ded5
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-04-07 17:16:43 +0530

    Feat: create new playlist

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,10 +1,43 @@
 import "./modal.css";
 import { useState } from "react";
+import { useAuth } from "../../context/authContext";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
 
 export const Modal = ({ setModal }) => {
   const [createPlaylist, setCreatePlaylist] = useState(false);
+  const {
+    auth: { token },
+  } = useAuth();
+  const [playlistName, setPlaylistName] = useState([]);
+
+  const [title, setTitle] = useState("");
+
+  const createPlaylistHandler = async () => {
+    const header = { authorization: token };
+    const data = {
+      title: title,
+      description: "this is my new playlist",
+    };
+    try {
+      const response = await axios.post(
+        "/api/user/playlists",
+        { playlist: data },
+        {
+          headers: header,
+        }
+      );
+      setPlaylistName(response.data.playlists);
+      setCreatePlaylist(false);
+    } catch (e) {
+      console.log(e.message);
+      toast.error("please Login first");
+    }
+  };
+
   return (
     <div className="modal-layer">
+      <ToastContainer />
       <div className="modal-box">
         <div className="modal-head">
           <span className="heading-lg">Add To Playlist</span>
@@ -13,22 +46,23 @@ export const Modal = ({ setModal }) => {
           </button>
         </div>
         <div className="modal-contant">
-          <label>
-            <input type="checkbox"></input>
-            My Playlist
-          </label>
-          <label>
-            <input type="checkbox"></input>
-            My Playlist Two
-          </label>
+          {playlistName.length > 0 &&
+            playlistName.map((playlist) => (
+              <label key={playlist._id}>
+                <input type="checkbox"></input>
+                {playlist.title}
+              </label>
+            ))}
+
           {createPlaylist && (
             <div className="playlist-box">
               <input
                 className="newplaylist-input"
                 type="text"
                 placeholder="create new playlist"
+                onChange={(e) => setTitle(e.target.value)}
               ></input>
-              <button className="btn" onClick={() => setCreatePlaylist(false)}>
+              <button className="btn" onClick={() => createPlaylistHandler()}>
                 Add
               </button>
             </div>
